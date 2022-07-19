d7690941ab9c336422966eb6bdee03933018e392
Dinesh763 <103160826+Dinesh763@users.noreply.github.com>
2022-07-19 15:47:51 +0530

    Update SampleGitHubProject.java

--- MavenExample/src/test/java/org/GitHub/Pratice/SampleGitHubProject.java
+++ MavenExample/src/test/java/org/GitHub/Pratice/SampleGitHubProject.java
@@ -8,7 +8,7 @@ public void GithubSample(){
 	System.out.println("DK");
 }
 @Test
-public void GithubSample1(){
+public void GithubSample1(){//again done
 	System.out.println("DKS");//Changes Done
 	System.out.println("Not done");//Changes made.
 	
