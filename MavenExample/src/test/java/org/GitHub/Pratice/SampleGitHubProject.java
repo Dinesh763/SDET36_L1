789b6a06c023fdcf36aa31f28111e1633d528fd8
user <user@DESKTOP-UJH33DH>
2022-07-19 12:08:14 +0530

    2nd Commit

--- MavenExample/src/test/java/org/GitHub/Pratice/SampleGitHubProject.java
+++ MavenExample/src/test/java/org/GitHub/Pratice/SampleGitHubProject.java
@@ -3,6 +3,10 @@ package org.GitHub.Pratice;
 import org.testng.annotations.Test;
 
 public class SampleGitHubProject {
-//@Test
-//public void 
+@Test
+public void GithubSample(){
+	System.out.println("DK");
+}
+
+
 }
