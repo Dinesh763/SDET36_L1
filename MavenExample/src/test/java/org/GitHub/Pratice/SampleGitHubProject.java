ddccbf4d417e2575a51ee79a3294cdbd704e5113
user <user@DESKTOP-UJH33DH>
2022-07-20 15:59:42 +0530

    21th commit

--- MavenExample/src/test/java/org/GitHub/Pratice/SampleGitHubProject.java
+++ MavenExample/src/test/java/org/GitHub/Pratice/SampleGitHubProject.java
@@ -6,12 +6,14 @@ public class SampleGitHubProject {
 @Test
 public void GithubSample(){
 	System.out.println("DK");//Again changes done
+	String browser=System.getProperty("browser");
+	String url=System.getProperty("url");
 	
 }
 @Test
 public void GithubSample1(){
 	System.out.println("DKS");//Changes Done
-	//ddddd
+	//ddddddddddddddddddddddddddddddddddddd
 	System.out.println("Done");
 }
 
