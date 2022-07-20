b62f3866e99967c16b1896c67ed5b0949a93fe5f
user <user@DESKTOP-UJH33DH>
2022-07-20 16:35:59 +0530

    27TH COMMIT

--- MavenExample/src/test/java/org/GitHub/Pratice/SampleGitHubProject.java
+++ MavenExample/src/test/java/org/GitHub/Pratice/SampleGitHubProject.java
@@ -5,15 +5,17 @@ import org.testng.annotations.Test;
 public class SampleGitHubProject {
 @Test
 public void GithubSample(){
-	System.out.println("DK");//Again changes done
-	String browser=System.getProperty("browser");
-	String url=System.getProperty("url");
-	
+	System.out.println("DK");
+	//Again changes done
+	String BROWSER=System.getProperty("browser");
+	String URL=System.getProperty("url");
+	System.out.println(BROWSER+""+URL);
 }
 @Test
 public void GithubSample1(){
-	System.out.println("DKS");//Changes Done
-	//dddddddddddddddddddddddddddddddddddddddd
+	System.out.println("DKS");
+	//Changes Done
+	
 	System.out.println("Done");
 }
 
